529c7af42d82396896b83f06b91e8cdad4badea5
Div-Denis <djw19910815@gmail.com>
2024-07-25 09:35:41 -0400

    Adding carmake and carmodel to github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,7 +15,8 @@ urlpatterns = [
     # path for dealer reviews view
     path(route="logout", view=views.logout_request, name='logout'),
 
-    # path for add a review view
+    # path for add a cars view
+    path(route='get_cars', view=views.get_cars, name='getcars')
     
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -32,8 +32,8 @@ DEBUG = True
 # ALLOWED_HOSTS = ['localhost','http://127.0.0.1:8000']
 # ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 # CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000']
-ALLOWED_HOSTS = ['https://djw19910815-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai', 'localhost', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://djw19910815-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['https://djw19910815-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai', 'localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://djw19910815-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
